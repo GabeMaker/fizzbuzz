838e90da1259269ffc6921b34ee279bd645d6de5
gabe <GabeMaker@users.noreply.github.com>
2015-01-29 17:47:55 +0000

    set new test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -12,6 +12,10 @@ describe 'Fizzbuzz' do
       expect(divisible_by_three?(1)).to be false
     end
 
+    it 'is divisible by 5' do
+      expect(divisible_by_five?(5)).to be true
+    end
+
   end
 
 end
