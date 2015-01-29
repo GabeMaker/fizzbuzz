ad3546ae433d1705d77d7a67e74ba8c128ca3196
Roi Driscoll <roi.driscoll@gmail.com>
2015-01-29 17:38:48 +0000

    Write counter test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -8,6 +8,10 @@ describe 'Fizzbuzz' do
       expect(divisible_by_three?(3)).to be true
     end
 
+    it 'is NOT divisible by 3' do
+      expect(divisible_by_three?(1)).to be false
+    end
+
   end
 
 end
